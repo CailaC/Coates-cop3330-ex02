34ff8fe3b3119079e48dad9e7479d745f3f7c8db
CailaC <caila.coates2010@gmail.com>
2021-09-12 17:11:34 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Caila Coates
+ */
+
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println( "What is the input string?" );
+        String str = input.next();
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++){
+            if(str.charAt(i) != ' ')
+                count++;
+        }
+
+        System.out.println(str + " has " + count + " characters.");
+    }
+}
